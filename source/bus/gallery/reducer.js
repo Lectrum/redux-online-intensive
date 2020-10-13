5bfe1da89c57f5abf0be2953734227be453620fb
RiskyWorks <zorg1995@yandex.ru>
2020-10-14 02:28:39 +0300

    Замечания исправлены

--- source/bus/gallery/reducer.js
+++ source/bus/gallery/reducer.js
@@ -5,29 +5,45 @@ import photo4 from '../../theme/assets/photos/4.jpeg';
 import { SHOW_NEXT_PHOTO, SHOW_PHOTO, SHOW_PREV_PHOTO } from './types';
 
 const galleryInitialState = {
-    photos:             [photo1, photo2, photo3, photo4],
-    selectedPhotoIndex: 0,
+    photos: [photo1, photo2, photo3, photo4],
+    selectedPhotoIndex: 0
 };
 
 export const galleryReducer = (state = galleryInitialState, action) => {
+    let nextIndex = 0;
+
     switch (action.type) {
         case SHOW_NEXT_PHOTO:
+            nextIndex =
+                state.selectedPhotoIndex + 1 > state.photos.length - 1
+                    ? state.selectedPhotoIndex
+                    : state.selectedPhotoIndex + 1;
+
             return {
                 ...state,
-                selectedPhotoIndex:
-                    (state.selectedPhotoIndex + 1) % state.photos.length,
+                selectedPhotoIndex: nextIndex
             };
         case SHOW_PREV_PHOTO:
+            nextIndex =
+                state.selectedPhotoIndex - 1 < 0
+                    ? state.selectedPhotoIndex
+                    : state.selectedPhotoIndex - 1;
+
             return {
                 ...state,
-                selectedPhotoIndex:
-                    (state.photos.length + (state.selectedPhotoIndex - 1)) %
-                    state.photos.length,
+                selectedPhotoIndex: nextIndex
             };
         case SHOW_PHOTO:
+            let requestedIndex = parseInt(action.payload);
+
+            nextIndex =
+                requestedIndex > state.photos.length - 1 || requestedIndex < 0
+                    ? state.selectedPhotoIndex
+                    : requestedIndex;
+
             return {
                 ...state,
-                selectedPhotoIndex: payload % state.photos.length,
+                selectedPhotoIndex: nextIndex
             };
         default: {
             return state;
